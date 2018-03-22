e7031ba5300adf0f1e028e35b7674bcb16e70ed1
Richard Young <richard@bnry.digital>
2018-03-22 10:48:07 +0200

    update async

--- async/index.test.js
+++ async/index.test.js
@@ -1,12 +1,41 @@
-import {runCallback, successfulPromise, unsuccessfulPromise} from './index';
+export const successfulPromise = () => {
+  return Promise.resolve('success');
+};
 
 test('promise resolves', () => {
   return expect(successfulPromise()).resolves.toBe('success')
 });
 
+
+
+
+
+
+
+
+
+
+export const unsuccessfulPromise = () => {
+  return Promise.reject('fail');
+};
+
 test('promise rejects', () => {
   return expect(unsuccessfulPromise()).rejects.toBe('fail')
 });
+
+
+
+
+
+
+
+
+
+
+
+export const runCallback = (callback) => {
+  callback('success')
+};
 
 test('callback returns success', (done) => {
   const callback = (result) => {
