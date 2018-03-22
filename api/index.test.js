871adca9bd568422d9b6392e364bfce483b0acd9
Richard Young <richard@bnry.digital>
2018-03-22 11:26:30 +0200

    update api

--- api/index.test.js
+++ api/index.test.js
@@ -3,7 +3,9 @@ import {app} from './index'
 
 test('returns 200 response code and correct name', async () => {
   const testApi = request(app);
+
   const response = await testApi.get('/user');
+
   expect(response.statusCode).toBe(200);
   expect(response.body.name).toBe('Chuck Norris');
 });
